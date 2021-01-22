49d2f8b1d41c553fc7bb405568c0cee21d8d3180
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-01-22 10:23:02 +0530

    Time: 35 ms (39.12%), Space: 50.8 MB (81.61%) - LeetHub

--- find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
+++ find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
@@ -1,34 +1,23 @@
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
-​
-        int []arr = new int[k];
-        int []minIndices = new int[nums.length];
-        minIndices[nums.length - 1] = nums.length - 1;
-        int minIndex = nums.length-1;
-​
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] <= nums[minIndex])
-                minIndex = i;
-            minIndices[i] = minIndex;
-        }
-​
-        int j = 0;
-​
-        for (int i = 0; i < k; i++) {
-            if ((nums.length - minIndices[j]) >= k - i) {
-                arr[i] = nums[minIndices[j]];
-                j = minIndices[j] + 1;
-            } else {
-                minIndex = j;
-                j++;
-                for ( ; j <= nums.length - k + i; j++) {
-                    if (nums[j] < nums[minIndex])
-                        minIndex = j;
-                }
-                arr[i] = nums[minIndex];
-                j = minIndex + 1;
-            }
-        }
-        return arr;
+        
+        Stack<Integer> stack=new Stack<>();
+        
+        for(int i=0;i<nums.length;i++) {
+            while(!stack.isEmpty() && stack.peek()>nums[i] &&
+                  nums.length-i>k-stack.size()) {
+                stack.pop();
+            }
+            if(stack.size()<k) {
+            stack.push(nums[i]);
+            }
+        }
+        
+        int []ans=new int[k];
+        
+        for(int i=k-1;i>=0;i--) {
+            ans[i]=stack.pop();
+        }
+        return ans;
     }
 }
