2bc4250587bd5d442f6408f41224c9d5360ecf02
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-04-09 08:43:46 +0530

    Time: 20 ms (56.85%), Space: 39.2 MB (97.78%) - LeetHub

--- determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,30 +1,31 @@
 class Solution {
-    public boolean closeStrings(String word1, String word2) {
-        
-        if(word1.length()!=word2.length())
-            return false;
-        
-        int []d1=new int[26];
-        int []d2=new int[26];
-        
-        for(int i=0;i<word1.length();i++) {
-            char c=word1.charAt(i);
-            d1[c-'a']++;
-        }
-        
-        for(int i=0;i<word2.length();i++) {
-            char c=word2.charAt(i);
-            d2[c-'a']++;
-        }
-        
-        int res=0;
-        
-        for(int i=0;i<26;i++) {
-            if((d1[i]==0 && d2[i]!=0) || (d2[i]==0 && d1[i]!=0))
-                return false;
-            res^=d1[i];
-            res^=d2[i];
-        }
-        return res==0;
-    }
+    public boolean closeStrings(String word1, String word2) {
+        
+        if(word1.length()!=word2.length())
+            return false;
+        
+        int []d1=new int[26];
+        int []d2=new int[26];
+        
+        for(int i=0;i<word1.length();i++) {
+            char c=word1.charAt(i);
+            d1[c-'a']++;
+        }
+        
+        for(int i=0;i<word2.length();i++) {
+            char c=word2.charAt(i);
+            d2[c-'a']++;
+        }
+        
+        int res=0;
+        
+        for(int i=0;i<26;i++) {
+            if((d1[i]==0 && d2[i]!=0) || (d2[i]==0 && d1[i]!=0))
+                return false;
+        }
+       Arrays.sort(d1);
+       Arrays.sort(d2);
+        
+      return Arrays.equals(d1,d2);
+    }
 }
