c0e70516e1b45f8e10707cd04de7fc9a46e0a3f4
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-05-13 10:02:45 +0530

    Time: 9 ms (71.08%), Space: 38.7 MB (94.44%) - LeetHub

--- minimum-window-substring/minimum-window-substring.java
+++ minimum-window-substring/minimum-window-substring.java
@@ -1,47 +1,44 @@
-public class Solution {
-public String minWindow(String s, String t) {
-    if(s == null || s.length() < t.length() || s.length() == 0){
-        return "";
-    }
-    HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-    for(char c : t.toCharArray()){
-        if(map.containsKey(c)){
-            map.put(c,map.get(c)+1);
-        }else{
-            map.put(c,1);
-        }
-    }
-    int left = 0;
-    int minLeft = 0;
-    int minLen = s.length()+1;
-    int count = 0;
-    for(int right = 0; right < s.length(); right++){
-        if(map.containsKey(s.charAt(right))){
-            map.put(s.charAt(right),map.get(s.charAt(right))-1);
-            if(map.get(s.charAt(right)) >= 0){
-                count ++;
-            }
-            while(count == t.length()){
-                if(right-left+1 < minLen){
-                    minLeft = left;
-                    minLen = right-left+1;
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        if(s.length()==0 || t.length()==0)
+            return "";
+        
+        HashMap<Character,Integer>map=new HashMap<>();
+        
+        char []str=s.toCharArray();
+        
+        for(char i:t.toCharArray())
+            map.put(i,map.getOrDefault(i,0)+1);
+        
+        int i=0,j=0,minLeft=0,minLen=Integer.MAX_VALUE,count=0;
+        
+        for(j=0;j<str.length;j++) {
+            
+            if(map.containsKey(str[j])) {
+                map.put(str[j],map.get(str[j])-1);
+                
+            if(map.get(str[j])>=0)
+                    ++count;
+            while(count==t.length()) {
+                
+                if(minLen>j-i+1) {
+                    minLeft=i;
+                    minLen=j-i+1;
                 }
-                if(map.containsKey(s.charAt(left))){
-                    map.put(s.charAt(left),map.get(s.charAt(left))+1);
-                    if(map.get(s.charAt(left)) > 0){
-                        count --;
-                    }
-                }
-                left ++ ;
+            
+            if(map.containsKey(str[i])) {
+                map.put(str[i],map.get(str[i])+1);
+                if(map.get(str[i])>0)
+                    count--;
+            }
+            i++;
             }
         }
+        }
+        
+        if(minLen>str.length)
+            return "";
+        return s.substring(minLeft,minLeft+minLen);
     }
-    if(minLen>s.length())  
-    {  
-        return "";  
-    }  
-    
-    return s.substring(minLeft,minLeft+minLen);
-}
-}
-
+} 
