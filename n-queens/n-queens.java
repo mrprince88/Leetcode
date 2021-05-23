112bee1932ccceb19c3eded5fd38e4e316962e6d
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-05-23 09:34:51 +0530

    Time: 4 ms (60.09%), Space: 42 MB (5.06%) - LeetHub

---
+++ n-queens/n-queens.java
@@ -0,0 +1,50 @@
+class Solution {
+    List<List<String>> list;
+    public List<List<String>> solveNQueens(int n) {
+        list=new ArrayList<>();
+        char [][]a=new char[n][n];
+        for(int i=0;i<n;i++)
+            Arrays.fill(a[i],'.');
+        nQueens(a,n,0);
+        return list;
+    }
+    
+    void nQueens(char [][]a,int n,int i) {
+        if(i==n) {
+            List<String> sol=getString(a,n);
+            list.add(sol);
+        }
+        for(int j=0;j<n;j++) {
+            if(isPossible(a,n,i,j)) {
+                a[i][j]='Q';
+                nQueens(a,n,i+1);
+                a[i][j]='.';
+            }
+        }
+    }
+    
+    List<String> getString(char [][]a,int n) {
+        List<String> sol=new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            StringBuffer sb=new StringBuffer();
+            sb.append(a[i]);
+            sol.add(sb.toString());
+        }
+        return sol;
+    }
+    
+    boolean isPossible(char [][]a,int n,int row,int col) {
+        for(int i=row-1;i>=0;i--)
+            if(a[i][col]=='Q')
+                return false;
+        
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--)
+            if(a[i][j]=='Q')
+                return false;
+        
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++)
+            if(a[i][j]=='Q')
+                return false;
+        return true;
+    }
+}
