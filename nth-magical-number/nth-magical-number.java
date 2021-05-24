268d2e7cb284f1ec43840353a84de8b0d8d6d357
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-05-24 18:38:50 +0530

    Time: 0 ms (100.00%), Space: 35.9 MB (24.56%) - LeetHub

--- nth-magical-number/nth-magical-number.java
+++ nth-magical-number/nth-magical-number.java
@@ -11,7 +11,7 @@ class Solution {
         long l=2,r=(long)(1e14),mod=(long)(1e9+7),lcm=a*b/A;
         
         while(l<r) {
-            long m=l+(r-l)/2;
+           long m=l+(r-l)/2;
             if((m/a) + (m/b) - (m/lcm)<n) l=m+1;
             else r=m;    
         }
