ba0e8e1b4d14a0d39fc4de5a993557351c294334
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-05-28 10:38:09 +0530

    Time: 0 ms (100.00%), Space: 36 MB (25.53%) - LeetHub

--- number-of-1-bits/number-of-1-bits.java
+++ number-of-1-bits/number-of-1-bits.java
@@ -1,13 +1,11 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        int mask = 1;
-        int count = 0;
-        for(int i=0;i<32;i++) {
-            if((n & mask) != 0)
-                count++;
-            mask <<= 1;
-        }
-        return count;
+    int sum = 0;
+    while (n != 0) {
+        sum++;
+        n &= (n - 1);
+    }
+    return sum;
     }
 }
