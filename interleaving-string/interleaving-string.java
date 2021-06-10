dd2c9acbae7b20d4f4f9d047ee1b91a1702a1817
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-06-10 10:49:18 +0530

    Time: 8 ms (24.59%), Space: 38.8 MB (41.95%) - LeetHub

--- interleaving-string/interleaving-string.java
+++ interleaving-string/interleaving-string.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int n=s1.length(),m=s2.length(),o=s3.length();
         
-        if(o!=n+m)
+        int n=s1.length(),m=s2.length(),k=s3.length();
+        if(n+m!=k)
             return false;
-        
-        boolean []dp=new boolean[m+1];
+        char []str1=s1.toCharArray(),str2=s2.toCharArray(),str3=s3.toCharArray();
+        boolean [][]dp=new boolean[n+1][m+1];
         
         for(int i=0;i<=n;i++) {
             for(int j=0;j<=m;j++) {
                 if(i==0 && j==0)
-                    dp[j]=true;
+                    dp[i][j]=true;
                 else if(i==0)
-                    dp[j]=dp[j-1] && s2.charAt(j-1) ==s3.charAt(i+j-1);
+                    dp[i][j]=(dp[i][j-1] && str2[j-1]==str3[j-1]);
                 else if(j==0)
-                    dp[j]=dp[j] && s1.charAt(i-1)==s3.charAt(i+j-1);
+                    dp[i][j]=(dp[i-1][j] && str3[i-1]==str1[i-1]);
                 else
-                    dp[j]=(dp[j-1] && s2.charAt(j-1)==s3.charAt(i+j-1)) || (dp[j] && s1.charAt(i-1)==s3.charAt(i+j-1));
+                    dp[i][j]=(dp[i-1][j] && str3[i+j-1]==str1[i-1]) || (dp[i][j-1] && str3[i+j-1]==str2[j-1]);
+                    
             }
         }
-        return dp[m];
+        return dp[n][m];
     }
 }
