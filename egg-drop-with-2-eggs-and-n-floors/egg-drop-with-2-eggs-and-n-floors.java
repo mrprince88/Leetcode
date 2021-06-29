e71b0edf5c93e2b9eb6693350dce44e90a88d481
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-06-29 11:53:07 +0530

    Time: 60 ms (22.47%), Space: 36.3 MB (58.25%) - LeetHub

--- egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.java
+++ egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.java
@@ -1,14 +1,39 @@
 class Solution {
+    
     public int twoEggDrop(int n) {
-        // x+(x-1)+(x-2)....1<=n
-        // x^2+x-2n<=0
+        int eggs=2;
         
-        int a=1,b=1,c=-2*n;
-        
-        int d=(b*b)-(4*a*c);
-        
-        double x=((-1 * b + Math.sqrt(d))/2);
-        
-        return (int)Math.ceil(x);
+        return solver(eggs,n,new int[eggs+1][n+1]);
     }
+    
+    int solver(int eggs,int floors,int [][]dp) {
+        if(eggs==1 || floors<=1) 
+            return floors;
+        
+        if(dp[eggs][floors]>0)
+            return dp[eggs][floors];
+        
+        int min=Integer.MAX_VALUE;
+        
+        for(int i=1;i<=floors;i++)
+            min=Math.min(min, 1 + Math.max(solver(eggs-1,i-1,dp), solver(eggs,floors-i,dp)));
+        
+        dp[eggs][floors]=min;
+        
+        return dp[eggs][floors];
+    }
+    
+//     public int twoEggDrop(int n) {
+//         // x+(x-1)+(x-2)....1<=n
+//         // x^2+x-2n<=0
+        
+//         int a=1,b=1,c=-2*n;
+        
+//         int d=(b*b)-(4*a*c);
+        
+//         double x=((-1 * b + Math.sqrt(d))/2);
+        
+//         return (int)Math.ceil(x);
+//     }
+    
 }
