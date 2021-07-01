6ac30cfc9161ba66dce98476ca3d2f42c3f559f9
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-07-01 12:29:45 +0530

    Time: 53 ms (88.81%), Space: 36.8 MB (97.90%) - LeetHub

--- sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -2,26 +2,20 @@ class Solution {
     public int beautySum(String s) {
         char []str=s.toCharArray();
         int ans=0;
-        List<int[]> list=new ArrayList<>();
         int []freq=new int[26];
-        list.add(new int[26]);
-        
-        for(char i: str) {
-            freq[i-'a']++;
-            list.add(Arrays.copyOf(freq,26));
-        }
         
         for(int i=0;i<str.length;i++) {
-            for(int j=i+1;j<str.length+1;j++) {
-                int[] left=list.get(i);
-                int[] right=list.get(j); 
-                
-                int max=-1,min=501;
+            
+            for(int k=0;k<26;k++) freq[k]=0;
+            
+            for(int j=i;j<str.length;j++) {
+                freq[str[j]-'a']++;
+                int max=-1,min=5001;
                 
                 for(int k=0;k<26;k++) {
-                    if(left[k]!=right[k]) {
-                    max=Math.max(max,right[k]-left[k]);
-                    min=Math.min(min,right[k]-left[k]);
+                    if(freq[k]!=0) {
+                        min=Math.min(min,freq[k]);
+                        max=Math.max(max,freq[k]);
                     }
                 }
                 ans+=max-min;
