dd5ac3b636ae6dd888ae0476056e4d80730f45af
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-07-01 19:16:01 +0530

    Time: 2 ms (99.39%), Space: 46.4 MB (67.34%) - LeetHub

--- gray-code/gray-code.java
+++ gray-code/gray-code.java
@@ -1,13 +1,31 @@
+class Solution {
+    int nums = 0;
+    public List<Integer> grayCode(int n) {
+        List<Integer> ret = new ArrayList<>();
+        backTrack(n, ret);
+        return ret;
+    }
+    
+    private void backTrack(int n, List<Integer> ret) {
+        if (n == 0) {
+            ret.add(nums);
+            return;
+        }
+        else {
+            backTrack(n - 1, ret);
+            nums ^= (1 << n - 1);
+            backTrack(n - 1, ret);
+        }
+    }
+    
+    
+}
+
+
 // class Solution {
 //     public List<Integer> grayCode(int n) {
-        
+//         ArrayList arr=new ArrayList<>();
+//         for(int i=0;i< 1<<n;i++) arr.add(i^(i>>1));
+//         return arr;
 //     }
 // }
-
-class Solution {
-    public List<Integer> grayCode(int n) {
-        ArrayList arr=new ArrayList<>();
-        for(int i=0;i< 1<<n;i++) arr.add(i^(i>>1));
-        return arr;
-    }
-}
