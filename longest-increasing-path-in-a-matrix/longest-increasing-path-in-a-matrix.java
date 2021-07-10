61bfb4c5d1fc7dc0287b334b4fb3b8c5d98fda4c
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-07-10 18:58:19 +0530

    Time: 11 ms (33.59%), Space: 51.8 MB (8.74%) - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,40 +1,36 @@
 class Solution {
-    
     int [][]dp;
-    
     public int longestIncreasingPath(int[][] matrix) {
-        
-        if(matrix==null || matrix.length==0)
-            return 0;
-        
         int n=matrix.length,m=matrix[0].length;
+        int ans=0;
         dp=new int[n][m];
         
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-                max = Math.max (max, DFS (matrix, i, j));
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                ans=Math.max(ans,dfs(matrix,i,j,-1));
+            }
         }
-        return max;
+        return ans;
     }
     
-    int DFS (int matrix [][], int i, int j) {
-        if (dp [i][j] > 0)
-            return dp [i][j];
-        int max = 1;
-        
-        if (i+1 < matrix.length && matrix [i+1][j] > matrix [i][j])
-            max = Math.max (max, 1 + DFS (matrix, i+1, j));
-        
-        if (i-1 >= 0 && matrix [i-1][j] > matrix [i][j])
-            max = Math.max (max, 1 + DFS (matrix, i-1, j));
-        
-        if (j+1 < matrix[0].length && matrix [i][j+1] > matrix [i][j])
-            max = Math.max (max, 1 + DFS (matrix, i, j+1));
-        
-        if (j-1 >= 0 && matrix [i][j-1] > matrix [i][j])
-            max = Math.max (max, 1 + DFS (matrix, i, j-1));
-        return dp [i][j] = max;
-    }
+    int dfs(int [][]matrix,int i,int j,int prev) {
+        if(i==-1 || j==-1 || i==matrix.length || j==matrix[0].length || matrix[i][j]<=prev) 
+            return 0;
 
+        if(dp[i][j]>0)
+            return dp[i][j];
+
+        int ans=0;
+        ans=Math.max(ans,dfs(matrix,i+1,j,matrix[i][j]));
+        
+        ans=Math.max(ans,dfs(matrix,i,j+1,matrix[i][j]));
+        
+        ans=Math.max(ans,dfs(matrix,i,j-1,matrix[i][j]));
+        
+        ans=Math.max(ans,dfs(matrix,i-1,j,matrix[i][j]));
+            
+        dp[i][j]=ans+1;
+            
+        return dp[i][j];
+    }
 }
