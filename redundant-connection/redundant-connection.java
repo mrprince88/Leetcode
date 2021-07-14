3ceebe6a9105acf178d30ffadc9d86149496b9ab
Prince Singh <51236601+mrprince88@users.noreply.github.com>
2021-07-14 12:01:29 +0530

    Time: 1 ms (63.41%), Space: 39.6 MB (29.71%) - LeetHub

--- redundant-connection/redundant-connection.java
+++ redundant-connection/redundant-connection.java
@@ -1,35 +1,45 @@
 class Solution {
     
-    Set<Integer> visited;
-    
-    public int[] findRedundantConnection(int[][] edges) {
-        Map<Integer,List<Integer>> graph=new HashMap<>();
-        int n=edges.length;
-        
-        for(int i=1;i<=n;i++)
-            graph.put(i,new ArrayList<>());
-        
-        for(int []i: edges) {
-            visited=new HashSet<>();
-            
-            if(!graph.get(i[0]).isEmpty() && !graph.get(i[1]).isEmpty() && dfs(graph,i[0],i[1]))
-                return i;
-            
-            graph.get(i[0]).add(i[1]);
-            graph.get(i[1]).add(i[0]);
+    class Node {
+        int parent,rank;
+        Node(int p,int r) {
+            parent=p; rank=r;
         }
-        
-        return new int[2];
     }
     
-    boolean dfs(Map<Integer, List<Integer>> graph,int source,int target) {
-        if(!visited.contains(source)) {
-            visited.add(source);
-            if(source==target) return true;
-            for(int i:graph.get(source))
-                if(dfs(graph,i,target))
-                    return true;
+    List<Node> dsuf;
+    public int[] findRedundantConnection(int[][] edges) {
+        
+        dsuf=new ArrayList<>();
+        
+        for(int i=0;i<edges.length;i++)
+            dsuf.add(new Node(-1,0));
+        
+        for(int[] edge: edges) {
+            int from=edge[0]-1,to=edge[1]-1;
+            if(find(from)!=find(to))
+                union(from,to);
+            else
+                return edge;
         }
-        return false;
+        return new int[0];
+    }
+    
+    int find(int i) {
+        if(dsuf.get(i).parent==-1)
+            return i;
+        return dsuf.get(i).parent=find(dsuf.get(i).parent);
+    }
+    
+    void union(int from,int to) {
+        Node a=dsuf.get(find(from)),b=dsuf.get(find(to));
+        if(a.rank>b.rank)
+            b.parent=from;
+        else if(a.rank<b.rank)
+            a.parent=to;
+        else {
+            a.parent=to;
+            a.rank++;
+        }
     }
 }
